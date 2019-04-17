0234e034d39366133d54773c4b14163f604425a5
Helena Rasche <hxr@hx42.org>
2019-04-17 21:01:51 +0200

    add colour replacement script

---
+++ frontmatter/images/colour-replace.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import random
+import re
+import sys
+import colorsys
+
+
+inv_color_variance = 10
+hue = float(sys.argv[2])
+saturation = (0.5, 0.7)
+saturation = (0.7, 1.0)
+value = (0.6, 1.0)
+
+
+def hsv2rgb(h,s,v):
+    return tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
+
+
+def color2():
+    hue_ = hue + (random.random() - 0.5) / inv_color_variance
+    if hue_ < 0:
+        hue_ = 1 - hue_
+
+    if hue_ > 1:
+        hue_ -= 1
+
+    (r, g, b) = hsv2rgb(hue_, random.uniform(*saturation), random.uniform(*value))
+    return f'{r:02x}{g:02x}{b:02x}'.format()
+
+
+
+with open(sys.argv[1], 'r') as handle:
+    for line in handle:
+        line = line.rstrip()
+
+        m1 = re.match(r'(.*[;"])fill:#ff0000(.*)stroke:#......(.*)', line)
+        m2 = re.match(r'(.*[;"])fill:#ff0000(.*)', line)
+        color = color2()
+        if m1:
+            print("%sfill:#%s%sstroke:#%s%s" % (m1.group(1), color, m1.group(2), color, m1.group(3)))
+        elif m2:
+            print("%sfill:#%s;stroke:#%s%s" % (m2.group(1), color, color, m2.group(2)))
+        else:
+            print(line)
