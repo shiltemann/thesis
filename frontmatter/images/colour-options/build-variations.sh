da2709b473b3a8c3004451acce99e4de0c5d08a9
Helena Rasche <hxr@hx42.org>
2019-10-24 11:03:18 +0200

    cleanup a bit

--- frontmatter/images/colour-options/build-variations.sh
+++ frontmatter/images/colour-options/build-variations.sh
@@ -4,11 +4,12 @@ for sat in {1..3}; do
 		echo $sat $val
 		for i in $(seq -w 00 05 95); do
 			mkdir -p sat-$sat-val-$val
-			python3 colour-replace.py cover-puzzle-ColorReplaceBase.svg 0.$i \
+			python3 ../inter/colour-replace.py cover-puzzle-ColorReplaceBase.svg 0.$i \
 				$(echo "(2.5 * $sat) / 10" | bc -l) \
 				$(echo "(2.5 * ($sat + 1)) / 10" | bc -l) \
 				$(echo "(2.5 * $val) / 10" | bc -l) \
 				$(echo "(2.5 * ($val + 1)) / 10" | bc -l) \
+				10 \
 			> sat-$sat-val-$val/out-$i.svg;
 		done
 	done
